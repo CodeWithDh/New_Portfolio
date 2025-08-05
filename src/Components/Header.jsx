8248d51a7a2cffcc0854ad4735ef9058736331a9
Shivam <codewithdh@gmail.com>
2025-08-06 01:51:37 +0530

    Creating my protfolio again my my own this time without Chat-gpt

---
+++ src/Components/Header.jsx
@@ -0,0 +1,20 @@
+import "./Header.css"
+
+export default function Header(){
+    return <>
+    <div className="Header">
+        <div className="name">
+            <h3>Shivam Dhinrga</h3>
+        </div>
+        <div className="head-bar">
+            <a className="head-item" href="#Home">Home</a>
+            <a className="head-item" href="#About">About</a>
+            <a className="head-item" href="#Experience">Experience</a>
+            <a className="head-item" href="#Skills">Skills</a>
+            <a className="head-item" href="#Projects">Projects</a>
+            <a className="head-item" href="#Contact">Contact</a>
+        </div>
+    </div>
+        <hr id="head-hr" />
+    </>
+}
